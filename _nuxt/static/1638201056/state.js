38a684241d19dd9a8494e045cbdedb2955dc9001
vamsee <4n0n5ur7@protonmail.com>
2021-11-30 09:20:01 +0530

    Add files via upload

---
+++ _nuxt/static/1638201056/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638201056",layout:"default",error:a,state:{menu:b,modal:{active:b,content:a},theme:a},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e1a5b430"}}}}(null,false,"\u002F"));
